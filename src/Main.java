03474e9861ea4c45c530441de4e01c0058e223a3
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-14 15:07:09 -0500

    added Vector2 class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Vector2 point1 = new Vector2(30.0f, 45.0f);
+        Vector2 point2 = new Vector2(10.0f, 20.0f);
+        Vector2 point3 = new Vector2();
+
+
+        System.out.println(point1.toString());
     }
 }
